c21ed0adcdd65cc02e54d44313385a4766750eb2
Sean McPherson <git@seanmcp.com>
2019-10-11 21:33:34 -0400

    Add bad example

--- examples/common.1.1-managing-focus-example.jsx
+++ examples/common.1.1-managing-focus-example.jsx
@@ -4,6 +4,22 @@ import DropdownMenuItems from 'components/DropdownMenuItems'
 // This is simplified for the sake of the presentation
 
 export default function DropdownMenu(props) {
+    const [isOpen, setIsOpen] = React.useState(false)
+    const open = () => setIsOpen(true)
+    const close = () => setIsOpen(false)
+
+    return (
+        <div className="DropdownMenu">
+            <button onClick={open}>
+                Menu
+            </button>
+            {isOpen && <DropdownMenuItems close={close} items={props.items} />}
+            {isOpen && <div className="Overlay" onClick={close} />}
+        </div>
+    )
+}
+
+export default function DropdownMenu(props) {
   const buttonRef = React.createRef()
   const [isOpen, setIsOpen] = React.useState(false)
   const open = () => setIsOpen(true)
@@ -22,6 +38,7 @@ export default function DropdownMenu(props) {
           items={props.items}
         />
       )}
+      {isOpen && <div className="Overlay" onClick={close} />}
     </div>
   )
 }
